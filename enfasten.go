37ac5ba8ac54257f366a00e261c95924dacce94a
Tristan Hume <tris.hume@gmail.com>
2017-09-30 11:27:56 -0400

    Hash all found image files

--- enfasten.go
+++ enfasten.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/sha256"
 	"flag"
 	"fmt"
 	"github.com/bmatcuk/doublestar"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,6 +23,7 @@ type config struct {
 
 func readConfig(basePath string) (conf config, err error) {
 	f, err := os.OpenFile(path.Join(basePath, "enfasten.yml"), os.O_RDONLY, 0)
+	defer f.Close()
 	if err != nil {
 		return
 	}
@@ -37,9 +40,44 @@ func readConfig(basePath string) (conf config, err error) {
 	return
 }
 
-func discoverImages(conf *config) {
-	matches, err := doublestar.Glob(path.Join(basePath, "**/*.{png,jpg}"))
-	fmt.Printf("%v", matches)
+func hashFile(path string) (hash []byte, err error) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err = io.Copy(h, f); err != nil {
+		return
+	}
+
+	hash = h.Sum(nil)
+	return
+}
+
+type foundImage struct {
+	Path string
+	Hash []byte
+}
+
+func discoverImages(inFolder string) (results []foundImage, err error) {
+	matches, err := doublestar.Glob(path.Join(inFolder, "**/*.{png,jpg}"))
+	if err != nil {
+		return
+	}
+	fmt.Printf("%v\n", matches)
+
+	for _, path := range matches {
+		var hash []byte
+		hash, err = hashFile(path)
+		if err != nil {
+			return
+		}
+		results = append(results, foundImage{path, hash})
+	}
+	fmt.Printf("%v\n", results)
+	return
 }
 
 func main() {
@@ -50,6 +88,6 @@ func main() {
 		log.Fatal(err)
 	}
 	conf.basePath = *basePath
-	fmt.Printf("%+v", conf)
-	discoverImages(&conf)
+	fmt.Printf("%+v\n", conf)
+	discoverImages(path.Join(conf.basePath, conf.InputFolder))
 }
